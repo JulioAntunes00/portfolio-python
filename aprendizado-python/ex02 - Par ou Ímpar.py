11e9e272be8c47d3dbe911f30c371bd87b451f0a
JulioAntunes00 <oliveira.ti@live.com>
2025-04-20 11:46:01 -0300

    Update ex02 - Par ou Ímpar.py

--- aprendizado-python/ex02 - Par ou Ímpar.py
+++ aprendizado-python/ex02 - Par ou Ímpar.py
@@ -1,6 +1,9 @@
-#Escreva um programa que receba um número e determine se ele é par ou ímpar.
+# Programa para verificar se um número é par ou ímpar
 
+# Solicita ao usuário que digite um número inteiro
 numero = int(input("Digite um número: "))
+
+# Verifica se o número é par (resto da divisão por 2 é 0)
 if numero %2 == 0:
     print("O número é par")
 else:
