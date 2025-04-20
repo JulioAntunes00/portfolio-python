9bf51d78d94d7193b313d10a23622c52acac0ca4
JulioAntunes00 <oliveira.ti@live.com>
2025-04-19 23:02:59 -0300

    Delete aprendizado-python directory

--- aprendizado-python/ex2 - loop para sair.py
+++
@@ -1,6 +0,0 @@
-#Crie um loop while que continue pedindo ao usuário para inserir uma palavra até que ele insira "sair".
-
-while True:
-    palavra = input("Insira uma palavra qualquer ou digite sair para encerrar:  ")
-    if palavra == "sair":
-        break
