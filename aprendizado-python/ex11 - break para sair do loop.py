7e4368831755e617a078a9d79653ac1a12c3cdc5
JulioAntunes00 <oliveira.ti@live.com>
2025-04-20 11:54:09 -0300

    Update ex11 - break para sair do loop.py

--- aprendizado-python/ex11 - break para sair do loop.py
+++ aprendizado-python/ex11 - break para sair do loop.py
@@ -1,9 +1,12 @@
-#Escreva um código que utilize break para sair de um loop infinito quando um número aleatório entre 1 e 10 seja igual a 7.
+# Programa que fica sorteando número até sair o 7 e aí ele para
 
 import random
 
+# Loop infinito
 while True:
+    # Sorteia um número de 1 a 10
     numero = random.randint(1,11)
     print(numero)
+    # Se o número for 7, o loop é encerrado
     if numero == 7:
         break
