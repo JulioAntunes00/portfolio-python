8f8ecd872dba28cb23465f786ad4cf1fe47677bf
JulioAntunes00 <oliveira.ti@live.com>
2025-04-20 11:50:23 -0300

    Update ex06 - Cálculo de IMC.py

--- aprendizado-python/ex06 - Cálculo de IMC.py
+++ aprendizado-python/ex06 - Cálculo de IMC.py
@@ -1,10 +1,15 @@
-#Peça ao usuário para inserir o peso (em kg) e a altura (em metros) e calcule o IMC usando a fórmula IMC = peso / 
+# Programa que calcula o IMC da pessoa
 
+# Pede o peso da pessoa
 peso = float(input("Insira seu peso: "))
+
+# Pede a altura
 altura = float(input("Insira sua altura: "))
 
+# Faz o cálculo do IMC
 imc = peso / (altura * altura)
 
+# Mostra se o IMC está dentro do normal ou não
 if imc >= 18.5 and imc <= 24.9:
     print(f"Seu IMC está bom: {imc:.2f}")
 else:
