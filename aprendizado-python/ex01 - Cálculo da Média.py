c563bead627536ecbd13f7b8c295dc85eb0068df
JulioAntunes00 <oliveira.ti@live.com>
2025-04-20 11:44:42 -0300

    Update ex01 - Cálculo da Média.py

--- aprendizado-python/ex01 - Cálculo da Média.py
+++ aprendizado-python/ex01 - Cálculo da Média.py
@@ -1,8 +1,12 @@
-#Escreva um programa que receba três notas de um aluno e calcule a média.
+# Programa para calcular a média de três notas
 
+# Solicita ao usuário que digite as três notas
 nota1 = int(input("Digite sua nota 1: "))
 nota2 = int(input("Digite sua note 2: "))
 nota3 = int(input("Digite sua nota 3: "))
 
+# Calcula a média das notas
 media = (nota1 + nota2 + nota3) / 3
+
+# Exibe a média com duas casas decimais
 print(f"Sua média é de: {media:.2f}") 
