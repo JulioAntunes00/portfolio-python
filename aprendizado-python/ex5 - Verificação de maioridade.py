c7a4c3a36a4bc83a8de87e171e0d48f5cbfb0e84
JulioAntunes00 <oliveira.ti@live.com>
2025-04-19 23:06:34 -0300

    Delete aprendizado-python directory

--- aprendizado-python/ex5 - Verificação de maioridade.py
+++
@@ -1,8 +0,0 @@
-#Peça ao usuário para inserir sua idade e informe se ele é maior ou menor de idade (considere 18 anos como maioridade).
-
-idade = int(input("Digite sua idade: "))
-
-if idade >= 18:
-    print("Parabéns, você é maior de idade!")
-else:
-    print("Você é menor de idade :(")
--- aprendizado-python/ex8 - Cálculo de desconto.py
+++
@@ -1,8 +0,0 @@
-#Peça ao usuário para digitar o valor de um produto e a porcentagem de desconto. Calcule e exiba o valor final com o desconto aplicado.
-
-valor = float(input("Digite o valor do produto: "))
-desconto = float(input("Digite a porcentagem de desconto: "))
-
-valor_final = valor - (valor * desconto / 100)
-
-print(f"Valor com desconto: R$ {valor_final:.2f}")
