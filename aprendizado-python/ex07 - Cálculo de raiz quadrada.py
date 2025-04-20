d8bc8c426b330bd5cf117a70cba008c9507ae2be
JulioAntunes00 <oliveira.ti@live.com>
2025-04-20 11:51:11 -0300

    Update ex07 - Cálculo de raiz quadrada.py

--- aprendizado-python/ex07 - Cálculo de raiz quadrada.py
+++ aprendizado-python/ex07 - Cálculo de raiz quadrada.py
@@ -1,8 +1,12 @@
-#Peça ao usuário para inserir um número e calcule a raiz quadrada. Use a função math.sqrt()
+# Programa que calcula a raiz quadrada de um número
 
 import math
 
+# Pede pro usuário digitar um número
 numero = float(input("Digite um número: "))
+
+# Usa a função sqrt do módulo math pra calcular a raiz
 raiz = math.sqrt(numero)
 
+# Mostra o resultado
 print(f"A raiz quadrada de {numero} é {raiz:.2f}")
