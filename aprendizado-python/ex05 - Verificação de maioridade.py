15753921d82907938b139d42a4d340daf4f007f7
JulioAntunes00 <oliveira.ti@live.com>
2025-04-20 11:49:20 -0300

    Update ex05 - Verificação de maioridade.py

--- aprendizado-python/ex05 - Verificação de maioridade.py
+++ aprendizado-python/ex05 - Verificação de maioridade.py
@@ -1,7 +1,9 @@
-#Peça ao usuário para inserir sua idade e informe se ele é maior ou menor de idade (considere 18 anos como maioridade).
+# Programa para verificar se a pessoa é maior de idade
 
+# Solicita a idade do usuário
 idade = int(input("Digite sua idade: "))
 
+# Verifica se a idade é maior ou igual a 18
 if idade >= 18:
     print("Parabéns, você é maior de idade!")
 else:
