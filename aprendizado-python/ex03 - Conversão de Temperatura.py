6eb044021f9298be7a47aa271d96c24daa4a15e4
JulioAntunes00 <oliveira.ti@live.com>
2025-04-20 11:46:52 -0300

    Update ex03 - Conversão de Temperatura.py

--- aprendizado-python/ex03 - Conversão de Temperatura.py
+++ aprendizado-python/ex03 - Conversão de Temperatura.py
@@ -1,7 +1,10 @@
-#Escreva um programa que converta uma temperatura de Celsius para Fahrenheit.
+# Programa para converter temperatura de Celsius para Fahrenheit
 
+# Solicita a temperatura em graus Celsius
 temp = float(input("Digite a temperarua em ºC: "))
+
+# Aplica a fórmula de conversão para Fahrenheit
 fahrenheit = (temp * 9/5)+32
 
+# Exibe o resultado com duas casas decimais
 print(f"{temp:.2f}°C é igual a {fahrenheit:.2f}°F")
-
