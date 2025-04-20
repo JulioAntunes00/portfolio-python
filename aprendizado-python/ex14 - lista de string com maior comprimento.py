3da8e8285e531d468504c9d6e188a6e68658d446
JulioAntunes00 <oliveira.ti@live.com>
2025-04-20 11:54:41 -0300

    Update ex14 - lista de string com maior comprimento.py

--- aprendizado-python/ex14 - lista de string com maior comprimento.py
+++ aprendizado-python/ex14 - lista de string com maior comprimento.py
@@ -1,11 +1,14 @@
-#Crie um loop for que percorra uma lista de strings e imprima a string de maior comprimento.
+# Programa que mostra qual palavra da lista é a mais longa
 
 lista = ["maçã", "banana", "abacaxi", "laranja", "morango"]
 
+# Começa com uma string vazia
 maior_lista = ""
 
+# Vai comparando o tamanho de cada palavra
 for s in lista:
     if len(s) > len(maior_lista):
         maior_lista = s
 
+# Mostra qual foi a maior
 print(f"O nome de maior comprimento é: {maior_lista}")
