1ba728eb761c36edb8ceba9a8b0aee520d585d52
JulioAntunes00 <oliveira.ti@live.com>
2025-04-20 11:54:31 -0300

    Update ex13 - calculo de 1 a 100 usando while.py

--- aprendizado-python/ex13 - calculo de 1 a 100 usando while.py
+++ aprendizado-python/ex13 - calculo de 1 a 100 usando while.py
@@ -1,10 +1,13 @@
-# Escreva um programa que use uma estrutura while para calcular a soma dos números de 1 a 100.
+# Programa que soma todos os números de 1 até 100
 
 soma = 0
 numero = 1
 
+# Enquanto o número for menor ou igual a 100
 while numero <= 100:
+    # Soma o número atual na variável soma
     soma += numero
     numero += 1
 
+# Mostra o total da soma
 print(f"A soma é: {soma}")
