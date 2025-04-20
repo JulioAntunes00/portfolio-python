e99d97d004305a1d2cbae22c35704fd0f4866a95
JulioAntunes00 <oliveira.ti@live.com>
2025-04-19 23:11:18 -0300

    Delete aprendizado-python directory

--- aprendizado-python/ex4 - Cálculo de área de um círculo.py
+++
@@ -1,6 +0,0 @@
-#Peça ao usuário para digitar o raio de um círculo e calcule a área. A fórmula da área é A = πr², onde π = 3.14159.
-
-raio = float(input("Digite o raio do círculo: "))
-pi = 3.14159
-area = pi * (raio ** 2)
-print(f"A área do círculo é: {area:.2f}")
