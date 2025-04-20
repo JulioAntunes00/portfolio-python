882244458cdaebbf53909acbc84ec04482799e93
JulioAntunes00 <oliveira.ti@live.com>
2025-04-20 11:51:25 -0300

    Update ex08 - Cálculo de desconto.py

--- aprendizado-python/ex08 - Cálculo de desconto.py
+++ aprendizado-python/ex08 - Cálculo de desconto.py
@@ -1,8 +1,13 @@
-#Peça ao usuário para digitar o valor de um produto e a porcentagem de desconto. Calcule e exiba o valor final com o desconto aplicado.
+# Programa pra calcular o valor com desconto
 
+# Pede o valor do produto
 valor = float(input("Digite o valor do produto: "))
+
+# Pede o percentual de desconto
 desconto = float(input("Digite a porcentagem de desconto: "))
 
+# Calcula o valor final com desconto aplicado
 valor_final = valor - (valor * desconto / 100)
 
+# Mostra o valor com desconto
 print(f"Valor com desconto: R$ {valor_final:.2f}")
