ed33f265963fa1ea130c8d6554c9c8468113c1a7
Julio Oliveira <oliveira.ti@live.com>
2025-09-19 15:35:53 -0300

    Update nota_media_escolar.cpp
    Adicionado mais 2 notas

--- aprendizado_C++/nota_media_escolar.cpp
+++ aprendizado_C++/nota_media_escolar.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
 int main()
 {
     float nota1;
     float nota2;
+    float nota3;
+    float nota4;
     float media;
     
-    cout<<"Qual sua nota 1?: \n";
-    cin>>nota1;
+    cout << "Qual sua nota 1?: \n";
+    cin >> nota1;
     
-    cout<<"Qual sua nota 2?: \n";
-    cin>>nota2;
+    cout << "Qual sua nota 2?: \n";
+    cin >> nota2;
     
-    media = (nota1 + nota2) / 2;
-    cout<<"Sua média foi de: "<<media<<"\n";
+    cout << "Qual sua nota3?: \n";
+    cin >> nota3;
     
-    if (media>=7)
-        {
-            cout<<"Aprovado";
-        }
+    cout << "Qual sua nota4?: \n";
+    cin >> nota4;
+    
+    media = (nota1 + nota2 + nota3 + nota4) / 4;
+    cout << "Sua media foi de: " << media << "\n";
+    
+    if (media >= 7)
+    {
+        cout << "Aprovado";
+    }
+    
+    else if (media >= 5 && media < 7)
+    {
+        cout << "Ficou de exame";
+    }
+    
     else
-        {
-            cout<<"Reprovado";
-        }
+    {
+        cout << "Reprovado";
+    }
     
-    system("PAUSE");
     return 0;
     
 }
